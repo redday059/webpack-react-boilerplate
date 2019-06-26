ab3322cf01a8d673f31a1d3cf99e4af475d2ce4b
redday059 <redday059@gmail.com>
2019-06-26 21:41:46 +0200

    splits webpack.config, adds cdn for react and react-dom, html template, css

---
+++ webpack.config.prod.js
@@ -0,0 +1,20 @@
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.config.base');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+module.exports = merge(baseConfig, {
+  mode: 'production',
+  plugins: [new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    openAnalyzer: false,
+    reportFilename: 'bundle_size.html',
+  })],
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  }
+});
+
+/*
+ $ open dist/bundle-sizes.html
+*/
